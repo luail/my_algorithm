081273c7e7acc5df53c6794df3db9760a72170a9
kojoonhyuk <kynia0506@naver.com>
2025-10-06 00:17:03 +0900

    [Silver IV] Title: 괄호, Time: 108 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,35 +1,37 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.util.*;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int n = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < n; i++) {
-            boolean answer = true;
-            String input = br.readLine();
+            String line = br.readLine();
+
             int count = 0;
-            for (int j = 0; j < input.length(); j++) {
-                if (input.charAt(j) == '(') {
+
+            String ans = "YES";
+
+            for (int j = 0; j < line.length(); j++) {
+                if (line.charAt(j) == '(') {
                     count++;
-                } else if (input.charAt(j) == ')') {
+                } else {
+                    if (count <= 0) {
+                        ans = "NO";
+                        break;
+                    }
                     count--;
                 }
-                if (count < 0) {
-                    System.out.println("NO");
-                    break;
-                }
             }
-            if (count == 0) {
-                System.out.println("YES");
-            } else if (count > 0) {
-                System.out.println("NO");
+
+            if (count > 0) {
+                ans = "NO";
             }
+
+            System.out.println(ans);
         }
     }
 }
